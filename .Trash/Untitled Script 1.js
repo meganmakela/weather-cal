611744ea3b79730027711ab606f8efef99d37484
meganmakela <64018716+meganmakela@users.noreply.github.com>
2020-12-27 09:44:14 -0600

    Empty trash.

--- .Trash/Untitled Script 1.js
+++
@@ -1,3 +0,0 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: orange; icon-glyph: magic;
--- .Trash/Untitled Script.js
+++
@@ -1,3 +0,0 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: red; icon-glyph: magic;
